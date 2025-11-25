c74d33994297983c2b3dfa6a4980e0e12217f2e3
Serhii Kyrylenko <32450732+Devepre@users.noreply.github.com>
2025-11-25 20:58:04 +0200

    init lane setup

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Serhii Kyrylenko on 25.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
